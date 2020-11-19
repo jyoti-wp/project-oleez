1cd431145aa8ca4c91f1df29da336024d097f95c
jyoti-wp <sim_s006@icloud.com>
2020-11-19 19:24:06 +0530

    landing page section carousel

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 <?php get_header(); ?>
 
 <div class="content">
+<?php get_template_part( 'template-parts/home/oleez-landing-section-carousel' ); ?>
 <?php get_template_part( 'template-parts/home/oleez-landing-section-news'); ?>
-<?php get_template_part( 'template-parts/home/oleez-landing-section-about' ); ?>
-<?php get_template_part( 'template-parts/home/about-us' ); ?>
 <?php get_template_part( 'template-parts/home/valuable-section'); ?>
 <?php get_template_part( 'template-parts/home/oleez-landing-partners-clients'); ?>
 <?php get_template_part( 'template-parts/home/oleez-landing-section-testimonials' ); ?>
--- template-parts/home/oleez-landing-section-carousel.php
+++ template-parts/home/oleez-landing-section-carousel.php
@@ -1,4 +1,4 @@
-<section class="oleez-landing-section-about">
+<section class="oleez-landing-section-carousel">
     <div class="wrapper">
         <div class="oleez-landing-section-content">
             <div class="oleez-landing-section-verticals wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
@@ -16,17 +16,17 @@
             <div class="row">
                 <div class="landing-about-feature" style="">
                     <img src="/assets/src/images/icon_1.svg" alt="document" class="about-feature-icon">
-                    <h5 class="about-feature-title">Market Research</h5>
+                    <h2 class="about-feature-title">Market Research</h5>
                     <p class="about-feature-description">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
                 </div>
                 <div class="landing-about-feature" style="">
                     <img src="assets/src/images/icon_2.svg" alt="document" class="about-feature-icon">
-                    <h5 class="about-feature-title">Wireframe/Sketch</h5>
+                    <h2 class="about-feature-title">Wireframe/Sketch</h5>
                     <p class="about-feature-description">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
                 </div>
                 <div class="landing-about-feature" style="">
                     <img src="assets/src/images/icon_3.svg" alt="document" class="about-feature-icon">
-                    <h5 class="about-feature-title">Creative Design</h5>
+                    <h2 class="about-feature-title">Creative Design</h5>
                     <p class="about-feature-description">Lorem Ipsum is simply text the printing and typesetting standard industry.</p>
                 </div>
             </div>
