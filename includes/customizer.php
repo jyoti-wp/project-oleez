e7ab08c319ca3e57db0956e37d6455ea0ecfc4f9
Imran <imranhsayed@gmail.com>
2021-01-20 21:19:29 +0530

    Add customizer

--- includes/customizer.php
+++ includes/customizer.php
@@ -35,6 +35,49 @@ function oleez_customize_register( \WP_Customize_Manager $wp_customize ) {
 			]
 		);
 	}
+
+
+
+
+	//=============
+
+	// Logo section.
+	$main_section_id = 'oleez_logos_section';
+	$wp_customize->add_section(
+		$main_section_id,
+		[
+			'title'       => esc_html__( 'Logos Section', 'oleez' ),
+			'description' => esc_html__( 'Upload Logos', 'oleez' ),
+		]
+	);
+
+	$logo_names = [
+		'Logo One' => 'logo_one',
+		'Logo Two' => 'logo_two',
+		'Logo Three' => 'logo_three',
+		'Logo Four' => 'logo_four',
+		'Logo Five' => 'logo_five',
+		'Logo Six' => 'logo_six'
+	];
+
+	foreach ( $logo_names as $logo_label => $logo_name ) {
+		$setting_id = sprintf( 'oleez_%s', $logo_name );
+
+		$wp_customize->add_setting( $setting_id );
+		$wp_customize->add_control(
+			new \WP_Customize_Image_Control(
+				$wp_customize,
+				$setting_id,
+				[
+					'label'    => esc_html__( $logo_label, 'oleez' ),
+					'section'  => $main_section_id,
+					'settings' => $setting_id,
+				]
+			)
+		);
+
+	}
+
 }
 
 add_action( 'customize_register', 'oleez_customize_register' );
